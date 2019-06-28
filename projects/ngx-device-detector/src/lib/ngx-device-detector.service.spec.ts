614cd8a627cb8d5df52673ccfbae07c3179aacf5
Ahsan Ayaz <ahsan.ubitian@gmail.com>
2019-06-29 02:49:04 +0500

    fix(ci): Fix DeviceDetectorService test

--- projects/ngx-device-detector/src/lib/ngx-device-detector.service.spec.ts
+++ projects/ngx-device-detector/src/lib/ngx-device-detector.service.spec.ts
@@ -3,7 +3,11 @@ import { TestBed } from '@angular/core/testing';
 import { DeviceDetectorService } from './ngx-device-detector.service';
 
 describe('DeviceDetectorService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      DeviceDetectorService
+    ]
+  }));
 
   it('should be created', () => {
     const service: DeviceDetectorService = TestBed.get(DeviceDetectorService);
