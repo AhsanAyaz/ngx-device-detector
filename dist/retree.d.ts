db03113d464caa4c2e25eb1e687d09c0992a2224
Bruno Binci <bruno.binci@bairesdev.com>
2020-03-17 16:44:41 -0300

    Remove dist and precommit

--- dist/retree.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by ahsanayaz on 08/11/2016.
- */
-export declare class ReTree {
-    constructor();
-    test(string: string, regex: any): any;
-    exec(string: string, regex: any): any;
-}
