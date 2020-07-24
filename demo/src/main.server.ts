09b0ff436dbd504f476fc764e49313832a6ab5d9
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-07-24 17:58:20 +0200

    release 1.4.6 :tada:

--- demo/src/main.server.ts
+++ demo/src/main.server.ts
@@ -8,5 +8,6 @@ if (environment.production) {
 
 export { AppServerModule } from './app/app.server.module';
 export { ngExpressEngine } from "@nguniversal/express-engine";
-export { provideModuleMap } from "@nguniversal/module-map-ngfactory-loader";
 
+
+export { renderModule, renderModuleFactory } from '@angular/platform-server';
