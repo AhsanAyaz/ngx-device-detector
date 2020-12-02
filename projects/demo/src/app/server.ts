3bd1b5e896bdf01e2a6301f5724a5f391d15e6d3
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-12-02 22:29:14 +0100

    feat(ssr-demo): add ssr demo

--- projects/demo/src/app/server.ts
+++ projects/demo/src/app/server.ts
@@ -3,9 +3,29 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { Inject, Injectable, Optional, PLATFORM_ID } from '@angular/core';
+import { REQUEST } from '@nguniversal/express-engine/tokens';
+import { Request } from 'express';
+import { DeviceDetectorService } from '../../../ngx-device-detector/src/public-api';
+import { isPlatformServer } from '@angular/common';
+
+@Injectable()
+export class UniversalDeviceDetectorService extends DeviceDetectorService {
+  constructor(@Inject(PLATFORM_ID) platformId: any, @Optional() @Inject(REQUEST) request: Request) {
+    super(platformId);
+    console.log('lol')
+    if (isPlatformServer(platformId)){
+      super.setDeviceInfo((request.headers['user-agent'] as string) || '');
+    }
+  }
+}
 
 @NgModule({
   imports: [AppModule, ServerModule],
   bootstrap: [AppComponent],
+  providers: [{
+    provide: DeviceDetectorService,
+    useClass: UniversalDeviceDetectorService
+  },]
 })
 export class AppServerModule {}
