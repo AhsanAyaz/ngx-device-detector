aeee5d274a1962343d03fb08777ea4bef16d16ac
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-12-03 06:37:13 +0100

    chore(ssr-demo): use package from registry

--- projects/demo/src/app/server.ts
+++ projects/demo/src/app/server.ts
@@ -6,14 +6,13 @@ import { AppComponent } from './app.component';
 import { Inject, Injectable, Optional, PLATFORM_ID } from '@angular/core';
 import { REQUEST } from '@nguniversal/express-engine/tokens';
 import { Request } from 'express';
-import { DeviceDetectorService } from '../../../ngx-device-detector/src/public-api';
+import { DeviceDetectorService } from 'ngx-device-detector';
 import { isPlatformServer } from '@angular/common';
 
 @Injectable()
 export class UniversalDeviceDetectorService extends DeviceDetectorService {
   constructor(@Inject(PLATFORM_ID) platformId: any, @Optional() @Inject(REQUEST) request: Request) {
     super(platformId);
-    console.log('lol')
     if (isPlatformServer(platformId)){
       super.setDeviceInfo((request.headers['user-agent'] as string) || '');
     }
