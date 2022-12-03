1e654938dc2da9c0218c6e815160e7725e9490be
Yberion <brandon.largeau@gmail.com>
2022-12-03 18:37:07 +0100

    chore: update to Angular 15

--- projects/demo/src/demo.ts
+++ projects/demo/src/demo.ts
@@ -1,10 +1,1 @@
-import { enableProdMode } from '@angular/core';
-
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
-
 export { AppServerModule } from './app/server';
-export { renderModule, renderModuleFactory } from '@angular/platform-server';
