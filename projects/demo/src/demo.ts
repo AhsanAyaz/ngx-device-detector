a4ef73a8f6cf7f799bd9e0526f30f2729f6c0122
Raghav Khunger <raghav@kokotree.com>
2023-01-17 23:15:16 +0530

    Merge branch 'master'

--- projects/demo/src/demo.ts
+++ projects/demo/src/demo.ts
@@ -1,10 +1,2 @@
-import { enableProdMode } from '@angular/core';
-
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
-
 export { AppServerModule } from './app/server';
 export { renderModuleFactory } from '@angular/platform-server';
