483af4627bf0ee139d1136f8653827ab53d71b60
Muhammad Ahsan Ayaz <ahsan.ayaz@scania.com>
2024-12-08 06:01:16 +0500

    chore(angular): update to v19

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component, VERSION } from '@angular/core';
 import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
   propsToShow = ['userAgent', 'os', 'browser', 'device', 'os_version', 'browser_version', 'deviceType', 'orientation'];
--- projects/demo/src/app/pipes/keys.pipe.ts
+++ projects/demo/src/app/pipes/keys.pipe.ts
@@ -1,7 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'keys',
+    name: 'keys',
+    standalone: false
 })
 export class KeysPipe implements PipeTransform {
   transform(value, props: string[] = []): any {
