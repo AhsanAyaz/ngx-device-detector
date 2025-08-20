42da35423428e0f5f7ade2ce21535d607da0e3bd
AhsanAyaz <AhsanAyaz@users.noreply.github.com>
2025-08-20 10:28:12 +0000

    deploy: 3834e7b288c779cecdf16325be64ed19388a3d8a

--- js/menu-wc.js
+++ js/menu-wc.js
@@ -24,8 +24,13 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                     <a data-type="chapter-link" href="index.html"><span class="icon ion-ios-home"></span>Getting started</a>
                     <ul class="links">
                         <li class="link">
-                            <a href="index.html" data-type="chapter-link">
+                            <a href="overview.html" data-type="chapter-link">
                                 <span class="icon ion-ios-keypad"></span>Overview
+                            </a>
+                        </li>
+                        <li class="link">
+                            <a href="index.html" data-type="chapter-link">
+                                <span class="icon ion-ios-paper"></span>README
                             </a>
                         </li>
                         <li class="link">
