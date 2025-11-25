bdbc81691652478b509b69b505ab63aad2baf04f
Timo Taubmann (Timeout) <timeout@rytrox.de>
2025-11-25 16:31:30 +0100

    Tidy up project

--- projects/demo/src/app/app.config.server.ts
+++ projects/demo/src/app/app.config.server.ts
@@ -1,11 +1,9 @@
 import { provideServerRendering } from '@angular/ssr';
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { mergeApplicationConfig, ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { appConfig } from './app.config';
 
 const serverConfig: ApplicationConfig = {
-  providers: [
-    provideServerRendering()
-  ]
+    providers: [provideServerRendering(), provideZonelessChangeDetection()],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
--- projects/demo/src/main.server.ts
+++ projects/demo/src/main.server.ts
@@ -1,15 +1,8 @@
-import { provideZonelessChangeDetection } from '@angular/core';
 import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
 const bootstrap = (context: BootstrapContext) =>
-    bootstrapApplication(
-        AppComponent,
-        {
-            ...config,
-            providers: [provideZonelessChangeDetection(), ...config.providers]
-        },
-        context);
+    bootstrapApplication(AppComponent, config, context);
 
 export default bootstrap;
