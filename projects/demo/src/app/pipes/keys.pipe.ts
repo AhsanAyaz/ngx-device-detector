b1dfdfbd70c75c81729028150ec4f2005666248f
Timo Taubmann (Timeout) <timeout@rytrox.de>
2025-11-25 16:25:35 +0100

    Replace TSLint with ESLint

--- projects/demo/src/app/pipes/keys.pipe.ts
+++ projects/demo/src/app/pipes/keys.pipe.ts
@@ -1,21 +1,25 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { DeviceInfo } from 'ngx-device-detector';
 
-@Pipe({ name: 'keys', standalone:true })
+@Pipe({
+    name: 'keys',
+    standalone: true,
+})
 export class KeysPipe implements PipeTransform {
-  transform(value, props: string[] = []): any {
-    const keys = [];
-    const noFilter = props.length === 0;
-    for (const key in value) {
-      if (noFilter) {
-        if (value.hasOwnProperty(key)) {
-          keys.push({ key, value: value[key] });
+    public transform(value: DeviceInfo, props: string[] = []): { key: string; value: unknown }[] {
+        const keys: { key: string; value: unknown }[] = [];
+        const noFilter = props.length === 0;
+
+        for (const key in value) {
+            if (noFilter) {
+                if (value.hasOwnProperty(key)) {
+                    keys.push({ key, value: value[key] });
+                }
+            } else if (props.includes(key)) {
+                keys.push({ key, value: value[key] });
+            }
         }
-      } else {
-        if (props.indexOf(key) !== -1) {
-          keys.push({ key, value: value[key] });
-        }
-      }
+
+        return keys;
     }
-    return keys;
-  }
 }
