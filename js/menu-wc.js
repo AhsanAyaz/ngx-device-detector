ab55b9922fb1cead0f8767cf073544c245f1edcc
AhsanAyaz <AhsanAyaz@users.noreply.github.com>
2026-06-08 14:04:25 +0000

    deploy: ba40981812c5e2d2c87523e9883eae2315ed0be0

--- js/menu-wc.js
+++ js/menu-wc.js
@@ -129,9 +129,6 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                         </div>
                         <ul class="links collapse " ${ isNormalMode ? 'id="classes-links"' : 'id="xs-classes-links"' }>
                             <li class="link">
-                                <a href="classes/AppPage.html" data-type="entity-link" >AppPage</a>
-                            </li>
-                            <li class="link">
                                 <a href="classes/ReTree.html" data-type="entity-link" >ReTree</a>
                             </li>
                         </ul>
